5e1607c91176cbc15f242c1ed38554a3e7ab14f2
wade2006 <1121157674@qq.com>
2020-11-18 10:15:32 +0800

    打包到githubpage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="wrap">
+    <div class="control">
+       <div>第{{n}}手</div>
+       <div>
+         <button @click="reset">
+           重新开始
+         </button>
+       </div>
+    </div>
+    <div class="chess">
+    <div class="row">
+    <Cell @click="onCellClick(0,$event)" v-bind:n="n" :isfinish="isfinish" />
+    <Cell  @click="onCellClick(1,$event)"  v-bind:n="n" :isfinish="isfinish"/>
+    <Cell  @click="onCellClick(2,$event)" v-bind:n="n" :isfinish="isfinish"/>
+    
   </div>
+   <div class="row">
+    <Cell  @click="onCellClick(3,$event)" v-bind:n="n" :isfinish="isfinish"/>
+    <Cell  @click="onCellClick(4,$event)" v-bind:n="n" :isfinish="isfinish"/>
+    <Cell  @click="onCellClick(5,$event)" v-bind:n="n" :isfinish="isfinish"/>
+
+  
+  </div>
+   <div class="row">
+   <Cell  @click="onCellClick(6,$event)" v-bind:n="n" :isfinish="isfinish"/>
+    <Cell  @click="onCellClick(7,$event)" v-bind:n="n" :isfinish="isfinish"/>
+    <Cell  @click="onCellClick(8,$event)" v-bind:n="n" :isfinish="isfinish"/>
+  </div>
+  <div class="footer">
+    <span v-if="isfinish">{{result}}胜利</span>
+    <div v-if="even">{{even}}</div>
+  </div>
+ </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Cell from "./components/Cell";
 export default {
-  name: 'App',
+  data(){
+    return {
+      n:0,
+      isfinish:false,
+      map:[   //二维数组存“x”或者“y”
+        [null,null,null],
+        [null,null,null],
+        [null,null,null],  
+      ],
+      result:null,
+      even:false
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Cell
+  },
+  methods:{
+    onCellClick(i,text){
+      console.log(`${i}值${text}`)
+      this.map[Math.floor(i/3)][i%3]=text;
+      this.n=this.n+1
+      this.tell()
+    },
+    tell(){
+     const {map,text}=this
+     for(let i=0;i<2;i++){
+       if(map[i][0]!=null&&map[i][0]==map[i][1]&&map[i][1]==map[i][2]){
+         this.result=map[i][0];
+         this.isfinish=true
+       }
+     }
+     for(let j=0;j<2;j++){
+       if(map[0][j]!=null&&map[0][j]==map[1][j]&&map[1][j]==map[2][j]){
+         this.result=map[0][j];
+         this.isfinish=true
 
+       }
+     }
+     if(map[0][0]!=null&&map[0][0]==map[1][1]&&map[1][1]==map[2][2]){
+         this.result=map[0][0];
+         this.isfinish=true
+
+     }
+     if(map[2][0]!=null&&map[2][0]==map[1][1]&&map[1][1]==map[0][2]){
+         this.result=map[2][0];
+         this.isfinish=true
+
+     }
+      if (map.flat(2).every(item => item !== null) && !this.isfinish) {
+        this.even = "平局";
+      }
+    },
+    reset(){
+      console.log(this.$children)
+      this.$children.map(item => {
+        item.status = false;
+        item.text = null;
+      });
+      this.map = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ];
+      this.result = null;
+      this.n = 0;
+      this.isfinish = false;
+    }
+  }
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.row{
+  display: flex;
+}
+.wrap{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.control{
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+  width: 310px;
+  margin-bottom: 5px;
+}
+.footer{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 25px;
 }
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="cell" v-on:click="onClickSelf">
+      <template v-if="status">{{ text }}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["n", "isfinish"],
+  data() {
+    return {
+      status: false,
+      text: null,
+    };
+  },
+  methods: {
+    onClickSelf() {
+      if (this.isfinish) {
+        return;
+      }
+      if (this.text != null) {
+        return;
+      }
+      this.status =true;
+      this.text = this.n % 2 === 0 ? "x" : "o";
+      this.$emit("click", this.text);
+    },
+  },
+};
+</script>
+<style>
+.cell {
+  border: 1px solid #000;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 80px;
+}
+</style>
